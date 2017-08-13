8d0eb55133cd798f40d7cbb3858a5a3b18027f89
Sergey Kostyuk <contact@s-kostyuk.me>
2017-08-13 23:20:11 +0300

    [WIP] Added a draft of ApiGateway class

---
+++ adpl/api/api_gateway.py
@@ -0,0 +1,7 @@
+class ApiGateway(object):
+    """
+    ApiGateway is a class which validates requests that came, checks permissions
+    and pass requests further to corresponding components (to execute some command
+    of fetch information os specific thing, for example)
+    """
+    pass
