9887ec2690986a72e29c262b69dac62b12bb9612
Sergey Kostyuk <contact@s-kostyuk.me>
2017-08-18 11:52:16 +0300

    A draft of Connection class

---
+++ dpl/connections/connection.py
@@ -0,0 +1,7 @@
+class Connection(object):
+    """
+    Connection is a class that abstracts the details of specific
+    protocol and way of communication with specific devices.
+    One connection can be used by different devices (thing).
+    """
+    pass
