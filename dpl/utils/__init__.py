08a0508464846e8c048bfa50b3237c6b783a980b
Sergey Kostyuk <contact@s-kostyuk.me>
2017-08-19 20:40:58 +0300

    Implemented custom JSON encoder with Enum support

--- dpl/utils/__init__.py
+++ dpl/utils/__init__.py
@@ -1,4 +1,5 @@
 from .generate_token import generate_token
 from .obj_to_dict import obj_to_dict
+from .json_enum_encoder import JsonEnumEncoder
 
-__all__ = ["generate_token", "obj_to_dict"]
+__all__ = ["generate_token", "obj_to_dict", "JsonEnumEncoder"]
---
+++ dpl/utils/json_enum_encoder.py
@@ -0,0 +1,22 @@
+# Include standard modules
+import json
+from enum import Enum
+from typing import Any
+
+# Include 3rd-party modules
+# Include DPL modules
+
+
+class JsonEnumEncoder(json.JSONEncoder):
+    def default(self, obj: Any) -> str:
+        """
+        Redefine 'default' method of JSONEncoder:
+            if object is enum variable - use it's name as value
+            else - try to serialize with default json encoder
+        :param obj: object to be serialized
+        :return: a string representation of specified object
+        """
+        if isinstance(obj, Enum):
+            return obj.name
+
+        return json.JSONEncoder.default(self, obj)
