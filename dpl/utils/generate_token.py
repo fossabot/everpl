46cbd0495a79eee8246ca3d15bc9980e52ea496b
Sergey Kostyuk <contact@s-kostyuk.me>
2017-08-20 15:25:32 +0300

    Fixed typo

--- dpl/utils/generate_token.py
+++ dpl/utils/generate_token.py
@@ -2,12 +2,12 @@ import os
 import binascii
 
 
-def generate_token(lenght: int = 40) -> bytes:
+def generate_token(length: int = 40) -> bytes:
     """
     Generates a random token with a specified length
     :return: bytes-encoded string
     """
-    n_of_bytes = lenght // 2
+    n_of_bytes = length // 2
 
     return binascii.hexlify(os.urandom(n_of_bytes))
 
