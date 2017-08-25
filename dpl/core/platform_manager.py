70d9e28311d7ebaac60579836facd32968f9db8a
Sergey Kostyuk <contact@s-kostyuk.me>
2017-08-25 23:34:42 +0300

    Added placements data to things

--- dpl/core/platform_manager.py
+++ dpl/core/platform_manager.py
@@ -86,6 +86,7 @@ class PlatformManager(object):
             thing_platform = item["platform"]
             thing_type = item["type"]
             thing_description = item["description"]
+            thing_placement = item["placement"]
             con_id = item["con_id"]
             con_params = item["con_params"]
 
@@ -119,7 +120,8 @@ class PlatformManager(object):
                 metadata={
                     "description": thing_description,
                     "type": thing_type,
-                    "id": thing_id
+                    "id": thing_id,
+                    "placement": thing_placement
                 }
             )
 
