8cd34168f3e01a840fd134ef64349959fc28f893
Sergey Kostyuk <contact@s-kostyuk.me>
2017-09-28 22:22:45 +0300

    Added a small note on generate_token utility function

--- dpl/utils/generate_token.py
+++ dpl/utils/generate_token.py
@@ -2,6 +2,7 @@ import os
 import binascii
 
 
+# FIXME: CC15: Return str instead of bytes
 def generate_token(length: int = 40) -> bytes:
     """
     Generates a random token with a specified length
