daf81e701f9823844720bbc29b3c08e629d52d58
Sergey Kostyuk <contact@s-kostyuk.me>
2017-09-29 19:51:22 +0300

    [TESTS] [WIP] Implemented tests for /placements/ endpoint

--- tests/api/test_rest_api_provider.py
+++ tests/api/test_rest_api_provider.py
@@ -294,5 +294,271 @@ class TestRestApiProvider(unittest.TestCase):
 
         self.loop.run_until_complete(body())
 
+    def test_get_placement(self):
+        test_placement_id = 'R1'
+        test_url = self.base_url + 'placements/' + test_placement_id
+        test_token = "nobody_cares"
+
+        test_headers = {'Authorization': test_token}
+
+        test_placement_mock = {"key1": "nobody cares"}
+
+        self.api_gateway_mock.get_placement = mock.Mock()
+        self.api_gateway_mock.get_placement.return_value = test_placement_mock
+
+        test_response_status = 200
+
+        async def body():
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                async with session.get(url=test_url, headers=test_headers) as resp:
+                    self.assertEqual(resp.status, test_response_status)
+                    response_body = await resp.json()
+
+                    self.assertEqual(
+                        response_body, test_placement_mock
+                    )
+
+        self.loop.run_until_complete(body())
+
+    def test_get_placement_not_found(self):
+        test_placement_id = 'R1'
+        test_url = self.base_url + 'placements/' + test_placement_id
+        test_token = "nobody_cares"
+
+        test_headers = {'Authorization': test_token}
+
+        def get_placement_side_effect(*args, **kwargs):
+            raise exceptions.PlacementNotFoundError
+
+        test_response_body = api_errors.ERROR_TEMPLATES[1005].to_dict()
+
+        self.api_gateway_mock.get_placement = mock.Mock()
+        self.api_gateway_mock.get_placement.side_effect = get_placement_side_effect
+
+        test_response_status = 404
+
+        async def body():
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                async with session.get(url=test_url, headers=test_headers) as resp:
+                    self.assertEqual(resp.status, test_response_status)
+                    response_body = await resp.json()
+
+                    self.assertEqual(
+                        response_body, test_response_body
+                    )
+
+        self.loop.run_until_complete(body())
+
+    def test_get_placement_missing_auth_reader(self):
+        test_placement_id = 'R1'
+        test_url = self.base_url + 'placements/' + test_placement_id
+
+        test_response_status = 401
+
+        test_response_body = api_errors.ERROR_TEMPLATES[2100].to_dict()
+
+        async def body():
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                async with session.get(url=test_url) as resp:
+                    self.assertEqual(resp.status, test_response_status)
+                    response_body = await resp.json()
+
+                    self.assertEqual(
+                        response_body, test_response_body
+                    )
+
+        self.loop.run_until_complete(body())
+
+    def test_get_placement_invalid_token(self):
+        test_placement_id = 'R1'
+        test_url = self.base_url + 'placements/' + test_placement_id
+        test_token = "nobody_cares"
+
+        test_headers = {'Authorization': test_token}
+
+        def get_placement_side_effect(*args, **kwargs):
+            raise exceptions.InvalidTokenError
+
+        test_response_body = api_errors.ERROR_TEMPLATES[2101].to_dict()
+
+        self.api_gateway_mock.get_placement = mock.Mock()
+        self.api_gateway_mock.get_placement.side_effect = get_placement_side_effect
+
+        test_response_status = 401
+
+        async def body():
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                async with session.get(url=test_url, headers=test_headers) as resp:
+                    self.assertEqual(resp.status, test_response_status)
+                    response_body = await resp.json()
+
+                    self.assertEqual(
+                        response_body, test_response_body
+                    )
+
+        self.loop.run_until_complete(body())
+
+    def test_get_placement_token_insufficient_permissions(self):
+        test_placement_id = 'R1'
+        test_url = self.base_url + 'placements/' + test_placement_id
+        test_token = "nobody_cares"
+
+        test_headers = {'Authorization': test_token}
+
+        def get_placement_side_effect(*args, **kwargs):
+            raise exceptions.PermissionDeniedForTokenError
+
+        test_response_body = api_errors.ERROR_TEMPLATES[2110].to_dict()
+
+        self.api_gateway_mock.get_placement = mock.Mock()
+        self.api_gateway_mock.get_placement.side_effect = get_placement_side_effect
+
+        test_response_status = 403
+
+        async def body():
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                async with session.get(url=test_url, headers=test_headers) as resp:
+                    self.assertEqual(resp.status, test_response_status)
+                    response_body = await resp.json()
+
+                    self.assertEqual(
+                        response_body['error_id'], test_response_body['error_id']
+                    )
+
+                    self.assertIn('devel_message', response_body)
+                    self.assertIn('user_message', response_body)
+                    self.assertIn('docs_url', response_body)
+
+        self.loop.run_until_complete(body())
+
+    def test_get_placements(self):
+        test_url = self.base_url + 'placements/'
+        test_token = "nobody_cares"
+
+        test_headers = {'Authorization': test_token}
+
+        test_placements_mock = [{"key1": "nobody cares"}, {"key1": "nobody cares 2"}]
+
+        self.api_gateway_mock.get_placements = mock.Mock()
+        self.api_gateway_mock.get_placements.return_value = test_placements_mock
+
+        test_response_status = 200
+
+        async def body():
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                async with session.get(url=test_url, headers=test_headers) as resp:
+                    self.assertEqual(resp.status, test_response_status)
+                    response_body = await resp.json()
+
+                    self.assertEqual(
+                        response_body, {"placements": test_placements_mock}
+                    )
+
+        self.loop.run_until_complete(body())
+
+    def test_get_placements_empty(self):
+        test_url = self.base_url + 'placements/'
+        test_token = "nobody_cares"
+
+        test_headers = {'Authorization': test_token}
+
+        test_placements_mock = []
+
+        self.api_gateway_mock.get_placements = mock.Mock()
+        self.api_gateway_mock.get_placements.return_value = test_placements_mock
+
+        test_response_status = 200
+
+        async def body():
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                async with session.get(url=test_url, headers=test_headers) as resp:
+                    self.assertEqual(resp.status, test_response_status)
+                    response_body = await resp.json()
+
+                    self.assertEqual(
+                        response_body, {"placements": test_placements_mock}
+                    )
+
+        self.loop.run_until_complete(body())
+
+    def test_get_placements_missing_auth_reader(self):
+        test_url = self.base_url + 'placements/'
+
+        test_response_status = 401
+
+        test_response_body = api_errors.ERROR_TEMPLATES[2100].to_dict()
+
+        async def body():
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                async with session.get(url=test_url) as resp:
+                    self.assertEqual(resp.status, test_response_status)
+                    response_body = await resp.json()
+
+                    self.assertEqual(
+                        response_body, test_response_body
+                    )
+
+        self.loop.run_until_complete(body())
+
+    def test_get_placements_invalid_token(self):
+        test_url = self.base_url + 'placements/'
+        test_token = "nobody_cares"
+
+        test_headers = {'Authorization': test_token}
+
+        def get_placements_side_effect(*args, **kwargs):
+            raise exceptions.InvalidTokenError
+
+        test_response_body = api_errors.ERROR_TEMPLATES[2101].to_dict()
+
+        self.api_gateway_mock.get_placements = mock.Mock()
+        self.api_gateway_mock.get_placements.side_effect = get_placements_side_effect
+
+        test_response_status = 401
+
+        async def body():
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                async with session.get(url=test_url, headers=test_headers) as resp:
+                    self.assertEqual(resp.status, test_response_status)
+                    response_body = await resp.json()
+
+                    self.assertEqual(
+                        response_body, test_response_body
+                    )
+
+        self.loop.run_until_complete(body())
+
+    def test_get_placements_token_insufficient_permissions(self):
+        test_url = self.base_url + 'placements/'
+        test_token = "nobody_cares"
+
+        test_headers = {'Authorization': test_token}
+
+        def get_placements_side_effect(*args, **kwargs):
+            raise exceptions.PermissionDeniedForTokenError
+
+        test_response_body = api_errors.ERROR_TEMPLATES[2110].to_dict()
+
+        self.api_gateway_mock.get_placements = mock.Mock()
+        self.api_gateway_mock.get_placements.side_effect = get_placements_side_effect
+
+        test_response_status = 403
+
+        async def body():
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                async with session.get(url=test_url, headers=test_headers) as resp:
+                    self.assertEqual(resp.status, test_response_status)
+                    response_body = await resp.json()
+
+                    self.assertEqual(
+                        response_body['error_id'], test_response_body['error_id']
+                    )
+
+                    self.assertIn('devel_message', response_body)
+                    self.assertIn('user_message', response_body)
+                    self.assertIn('docs_url', response_body)
+
+        self.loop.run_until_complete(body())
+
 if __name__ == '__main__':
     unittest.main()
