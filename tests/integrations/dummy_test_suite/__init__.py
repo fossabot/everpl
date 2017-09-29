b64670dca301c12123bf9c28287bbb52c817b748
Sergey Kostyuk <contact@s-kostyuk.me>
2017-09-29 21:06:22 +0300

    [TESTS] [WIP] Created a package with dummy integration test suite

---
+++ tests/integrations/dummy_test_suite/__init__.py
@@ -0,0 +1,10 @@
+"""
+Dummy test suite is an example set of unit tests for your new integrations.
+
+Test suite for integration includes:
+- unit tests for all provided implementations Thing and its subtypes
+- unit tests for used Connection type (optionally)
+
+And those test suits are related only to one integration package (dummy
+in this case).
+"""
