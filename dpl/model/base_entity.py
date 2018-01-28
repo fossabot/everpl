91f0319c294a9b17278ab9603192251035af408c
Sergey Kostyuk <contact@s-kostyuk.me>
2018-01-28 17:47:22 +0200

    WIP: A draft of the base Entity class

---
+++ dpl/model/base_entity.py
@@ -0,0 +1,24 @@
+TDomainId = str
+
+
+class BaseEntity(object):
+    """
+    A declaration of Entity - base class for all Entities of Object
+    Model (like Things, Placements and Users)
+    """
+    def __init__(self, domain_id: TDomainId):
+        """
+        Constructor. Just saves domain_id to the internal variable
+
+        :param domain_id: an unique identifier of this Entity
+        """
+        self._domain_id = domain_id
+
+    @property
+    def domain_id(self) -> TDomainId:
+        """
+        Returns an unique identifier of this Entity
+
+        :return: an unique identifier of this Entity
+        """
+        return self._domain_id
