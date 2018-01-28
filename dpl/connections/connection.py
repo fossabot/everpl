342ae8d7510168913bf6f2c1d557903ae5cd4dc3
Sergey Kostyuk <contact@s-kostyuk.me>
2018-01-28 17:51:33 +0200

    BREAKING: Added domain_id value to Connection costructor and a corresponding property

--- dpl/connections/connection.py
+++ dpl/connections/connection.py
@@ -1,4 +1,7 @@
-class Connection(object):
+from dpl.model.base_entity import BaseEntity
+
+
+class Connection(BaseEntity):
     """
     Connection is a class that abstracts the details of specific
     protocol and way of communication with specific devices.
