0d1f9ebe0bd55d7f65a6ce4285a2afe3076e35a3
Sergey Kostyuk <contact@s-kostyuk.me>
2018-02-07 12:32:38 +0200

    WIP: Added a note about state preservations and bianry states in IState interface

--- dpl/things/capabilities/istate.py
+++ dpl/things/capabilities/istate.py
@@ -7,7 +7,21 @@ class IState(object):
 
     All implementations of this interface must to have a 'state'
     property and declare a concrete list (enumeration) of all
-    states possible
+    states possible.
+
+    Also it's needed to mention that all states must to be clearly
+    mapped to a one of a binary states: either 'active' or 'inactive'.
+    Such binary representation of a state can be read from an
+    'is_active' property. And different Actuators can provide own
+    methods for switching between an active and inactive states.
+
+    'unknown' state is considered as an 'inactive' binary state.
+
+    Things are must to preserve their last known state even after
+    they become unavailable. And all client classes must to be ready
+    to this fact (i.e. analyze a time of the last update in last_updated
+    property, analyze an availability status in is_available property
+    and so on).
     """
     class States(Enum):
         """
