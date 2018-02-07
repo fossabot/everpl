5008c322e42ca4633282222c139f7f18b516f10a
Sergey Kostyuk <contact@s-kostyuk.me>
2018-02-07 17:00:24 +0200

    WIP: Added an ILastUpdated capability

---
+++ dpl/things/capabilities/ilast_updated.py
@@ -0,0 +1,14 @@
+class ILastUpdated(object):
+    """
+    ILastUpdated capability allows to determine a moment
+    in time when the properties of a Thing was altered
+    for the last time.
+    """
+    @property
+    def last_updated(self) -> float:
+        """
+        Returns a timestamp of the last thing state update
+
+        :return: float, UNIX time
+        """
+        raise NotImplementedError()
