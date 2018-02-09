330743e573d56175af7eb3f047ba23192ba466b7
Sergey Kostyuk <contact@s-kostyuk.me>
2018-02-09 14:30:40 +0200

    WIP: A draft definiton of the UserService interface

---
+++ dpl/services/abs_user_service.py
@@ -0,0 +1,33 @@
+from dpl.model.domain_id import TDomainId
+from dpl.dtos.user_dto import UserDto
+from .abs_entity_service import AbsEntityService
+
+
+class AbsUserService(AbsEntityService[UserDto]):
+    """
+    A base class for all UserService implementations
+    """
+    def create_user(self, username: str, password: str) -> TDomainId:
+        """
+        Creates a new user in the system with the specified
+        username and password. Password value must NOT to be
+        saved as a plain text
+
+        :param username: username, login name of the User
+               to be created
+        :param password: password of the User to be created
+        :return: an unique identifier of the User
+        """
+        raise NotImplementedError()
+
+    def change_username(self, of_user: TDomainId, new_username: str) -> None:
+        """
+        Allows to change the username for the specified User
+
+        :param of_user: an identifier of the User (NOT username
+               but domain identifier) to be altered
+        :param new_username: new username to be set for the User
+        :return: None
+        """
+        raise NotImplementedError()
+
