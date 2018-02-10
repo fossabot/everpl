1b4d746617d7a6e42a28f9dca23ea51811b48849
Sergey Kostyuk <contact@s-kostyuk.me>
2018-02-10 11:50:00 +0200

    BREAKING: Dropped an old auth subsystem

--- dpl/auth/__init__.py
+++
@@ -1,4 +0,0 @@
-from .client import Client
-from .auth_manager import AuthManager
-
-__all__ = ["Client", "AuthManager"]
--- dpl/auth/client.py
+++
@@ -1,6 +0,0 @@
-class Client(object):
-    """
-    Client is a class that stores information about a registered client device
-    """
-    pass
-
