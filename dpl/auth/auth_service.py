8d6b406757bdb31007ceed1aeaf8b77a54738cd8
Sergey Kostyuk <contact@s-kostyuk.me>
2018-02-11 12:55:08 +0200

    WIP: A draft AuthService implementation

---
+++ dpl/auth/auth_service.py
@@ -0,0 +1,143 @@
+from dpl.services.service_exceptions import ServiceEntityResolutionError
+from dpl.services.abs_user_service import AbsUserService
+from dpl.services.abs_session_service import AbsSessionService
+
+from .abs_auth_service import (
+    AbsAuthService,
+    AuthInvalidUserPasswordCombinationError,
+    AuthInvalidTokenError,
+    AuthInsufficientPrivilegesError
+)
+
+
+class AuthService(AbsAuthService):
+    """
+    An implementation of the AbsAuthService interface
+    """
+    def __init__(self, user_service: AbsUserService, session_service: AbsSessionService):
+        """
+        Constructor. Receives instances of UserService and
+        SessionService to be used to control User's passwords
+        and their Sessions
+
+        :param user_service: an instance of UserService
+        :param session_service: an instance of SessionService
+        """
+        self._user_service = user_service
+        self._session_service = session_service
+
+    def request_access(
+            self, username: str, password: str, client_info: str,
+            client_ip: str
+    ) -> str:
+        """
+        Verifies username-password combination, initializes a
+        new Session for a client device or application and,
+        if everything was successful, returns a generated access
+        token
+
+        :param username: username of the User to login
+        :param password: password of the User to login
+        :param client_info: information about client device or
+               application which requested access on behalf of the
+               specified User
+        :param client_ip: a network address (IP) of a device from
+               which the access was requested
+        :return: an access token to be used by client device or
+                 application to access to the system
+        :raises AuthInvalidUserPasswordCombinationError:
+                if the specified username-password combination is
+                invalid
+        """
+        user_dto = self._user_service.authenticate(username, password)
+
+        session_id = self._session_service.create_session(
+            user_id=user_dto['domain_id'],
+            client_info=client_info,
+            client_ip=client_ip
+        )
+
+        access_token = self._session_service.get_access_key(session_id)
+
+        return access_token
+
+    def change_password(self, access_token: str, old_password: str, new_password: str) -> None:
+        """
+        Changes a password of the User from old_password to the
+        new_password. access_token parameter is used for User
+        identification and authorization.
+
+        WARNING: This method revokes all User's Sessions except
+        the current one
+
+        :param access_token: an access token which identifies
+               the User and grants an access to change User's
+               password
+        :param old_password: an old password of the User
+        :param new_password: a password to be set
+        :return: None
+        :raises AuthInvalidTokenError: if the specified access
+                token is invalid (i.e. was revoked or not
+                existing at all)
+        :raises AuthInsufficientPrivilegesError: if the specified
+                access token doesn't permit password changing
+        :raises AuthInvalidUserPasswordCombinationError:
+                if the specified old password value is invalid
+        """
+        session_dto = self._session_service.view_by_access_key(access_token)
+        user_id = session_dto['user_id']
+
+        self._user_service.change_password(
+            of_user=user_id,
+            old_password=old_password,
+            new_password=new_password
+        )
+
+    # FIXME: Implement a check_permission method
+
+    def close_session(self, access_token: str) -> None:
+        """
+        Closes current session (i.e. performs a logout).
+
+        Returns silently (i.e. without exceptions) if the specified
+        access token was revoked already or not was not existing
+        at all.
+
+        :param access_token: an access token of the current Session
+        :return: None
+        """
+        try:
+            session_dto = self._session_service.view_by_access_key(access_token)
+            session_id = session_dto['domain_id']
+            self._session_service.remove(domain_id=session_id)
+
+        except ServiceEntityResolutionError:
+            pass
+
+    def close_all_other_sessions(self, access_token: str) -> None:
+        """
+        Closes (i.e. deletes) all sessions for the User except
+        the current one (except the Session with the specified
+        access token)
+
+        :param access_token: an access token of the current Session
+        :return: None
+        :raises AuthInvalidTokenError: if the specified access
+                token is invalid (i.e. was revoked or not
+                existing at all)
+        :raises AuthInsufficientPrivilegesError: if the specified
+                access token doesn't permit session revocation
+        """
+        try:
+            session_dto = self._session_service.view_by_access_key(access_token)
+
+        except ServiceEntityResolutionError as e:
+            raise AuthInvalidTokenError from e
+
+        session_id = session_dto['domain_id']
+        user_id = session_dto['user_id']
+
+        self._session_service.close_all_user_sessions(
+            for_user=user_id,
+            excluding=session_id
+        )
