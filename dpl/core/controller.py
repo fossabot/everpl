fossabot/everpl

commit 922c3ec42328598f860057df488c4a75654f30ce

TEMP: Moved init of bindings to the Controller's consructor

dpl/core/controller.py
@@ -113,6 +113,25 @@ class Controller(object):
         self._connection_repo = ConnectionRepository()
         self._thing_repo = ThingRepository()
 
+        is_safe_mode = self._core_config['is_safe_mode']
+
+        if is_safe_mode:
+            module_logger.warning(
+                "\n\n\nSafe mode is enabled, the most of everpl capabilities will be disabled\n\n")
+            module_logger.warning(
+                "\n!!! REST API access will be enabled in the safe mode !!!\n")
+
+            # Only REST API will be enabled in the safe mode
+            self._apis_config['enabled_apis'] = ('rest_api',)
+
+            # Force enable API access
+            self._core_config['is_api_enabled'] = True
+        else:
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(
+                self._bootstrap_integrations()
+            )
+
         self._user_service_raw = UserService(self._user_repo)
         self._session_service_raw = SessionService(self._session_repo)
         self._auth_service = AuthService(self._user_service_raw, self._session_service_raw)
@@ -240,20 +259,6 @@ class Controller(object):
             self._apis_config['rest_api']['port'] = args.rest_api_port
 
     async def start(self):
-        is_safe_mode = self._core_config['is_safe_mode']
-
-        if is_safe_mode:
-            module_logger.warning("\n\n\nSafe mode is enabled, the most of everpl capabilities will be disabled\n\n")
-            module_logger.warning("\n!!! REST API access will be enabled in the safe mode !!!\n")
-
-            # Only REST API will be enabled in the safe mode
-            self._apis_config['enabled_apis'] = ('rest_api', )
-
-            # Force enable API access
-            self._core_config['is_api_enabled'] = True
-        else:
-            await self._bootstrap_integrations()
-
         # FIXME: Only for testing purposes
         try:
             self._user_service_raw.view_by_username('admin')

commit 0ebbe8396cf8fc4bb0c6bedbb2f192a022d1b78f

FIX: Fixed a startup error caused by 922c3ec4

dpl/core/controller.py
@@ -267,6 +267,8 @@ class Controller(object):
             self._user_service_raw.create_user("admin", "admin")
             self._db_session_manager.get_session().commit()
 
+        self._thing_service_raw.enable_all()
+
         is_api_enabled = self._core_config['is_api_enabled']
 
         if is_api_enabled:
@@ -316,8 +318,6 @@ class Controller(object):
         binding_bootstrapper.init_things(thing_settings)
 
         self._db_session_manager.remove_session()
-
-        self._thing_service_raw.enable_all()
 
     async def shutdown(self):
         await self._rest_api.shutdown_server()