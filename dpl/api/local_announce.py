0eebb8a97f57ff70d7d62e82a0cae49f90d16a51
Sergey Kostyuk <contact@s-kostyuk.me>
2018-03-11 19:53:33 +0200

    WIP: Started working on a local network announcement

---
+++ dpl/api/local_announce.py
@@ -0,0 +1,12 @@
+"""
+This module contains an implementation of everpl announcement logic for
+local networks
+"""
+
+
+class LocalAnnounce(object):
+    """
+    This class controls the announcement of everpl instance presence in the
+    local network using Zeroconf (Avahi / Bonjour) protocol
+    """
+    pass
