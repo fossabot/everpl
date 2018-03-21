7dc5f169e466a700b925caba6cdb14b4b09d470c
Sergey Kostyuk <contact@s-kostyuk.me>
2018-03-21 11:18:33 +0200

    WIP: Made publicly accessable Capabilities package-public

--- dpl/things/capabilities/__init__.py
+++ dpl/things/capabilities/__init__.py
@@ -3,3 +3,16 @@ This package contains definitions of different capabilities,
 different interfaces like a set of properties and methods,
 that can be provided by different implementations of a Thing
 """
+
+from .actuator import Actuator
+from .has_state import HasState
+from .is_active import IsActive
+from .on_off import OnOff
+from .open_closed import OpenClosed
+from .has_value import HasValue
+
+
+__all__ = (
+    'Actuator', 'HasState', 'HasValue', 'IsActive',
+    'OnOff', 'OpenClosed', 'HasValue'
+)
