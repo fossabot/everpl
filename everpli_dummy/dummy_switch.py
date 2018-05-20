d1ed899979b045f3c4698892981340c343d7e073
Sergey Kostyuk <contact@s-kostyuk.me>
2018-05-20 10:29:39 +0300

    BREAKING: Updated Dummy Switch to register with new Thing types

--- everpli_dummy/dummy_switch.py
+++ everpli_dummy/dummy_switch.py
@@ -119,6 +119,18 @@ class DummySwitchFactory(ThingFactory):
 
 ThingRegistry.register_factory(
     integration_name="dummy",
-    thing_type="switch",
+    thing_type="light",
+    factory=DummySwitchFactory()
+)
+
+ThingRegistry.register_factory(
+    integration_name="dummy",
+    thing_type="power_switch",
+    factory=DummySwitchFactory()
+)
+
+ThingRegistry.register_factory(
+    integration_name="dummy",
+    thing_type="fan",
     factory=DummySwitchFactory()
 )
